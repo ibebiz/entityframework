d33f68943a5b4fc71aee2f40ff9299f3814fc921
nguyennhathai <nguyennhathai03@gmail.com>
2016-11-16 09:24:00 +0700

    Hai

---
+++ Entityframework.HAI.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Entityframework.HAI._1.MvcApplication" Language="C#" %>
---
+++ Entityframework.HAI.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Entityframework.HAI._1.Startup))]
+namespace Entityframework.HAI._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
