31351699a0085e6ac235ec293b014c796c3c7883
nguyennhathai <nguyennhathai03@gmail.com>
2016-11-28 15:00:05 +0700

    Hoàn Thành T135

---
+++ AngularIntro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularIntro.Startup))]
+namespace AngularIntro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
