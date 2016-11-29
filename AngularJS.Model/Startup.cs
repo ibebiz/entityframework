0ff1bb4840f1d9c92ee2517776e606fcdc81fdce
nguyennhathai <nguyennhathai03@gmail.com>
2016-11-29 10:15:00 +0700

    Hoàn Thành T139

---
+++ AngularJS.Model/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJS.Model.Startup))]
+namespace AngularJS.Model
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
