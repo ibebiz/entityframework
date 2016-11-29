71f6f636e76799e57eb13fe84c7209b88d0a7ea3
nguyennhathai <nguyennhathai03@gmail.com>
2016-11-29 12:06:00 +0700

    Hoàn Thành T141

---
+++ AngularJS.Controllers/Scripts/personController.js
@@ -0,0 +1,9 @@
+﻿
+       var app = angular.module('myApp', []);
+       app.controller('namesCtrl', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function () {
+        return $scope.firstName + " " + $scope.lastName;
+    };
+});
---
+++ AngularJS.Controllers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJS.Controllers.Startup))]
+namespace AngularJS.Controllers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
